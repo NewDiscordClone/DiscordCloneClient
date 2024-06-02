1187d50c75754b5e4a8f73d22b722e7dbb630798
Grabtot <dneshotkin@gmail.com>
2024-06-02 20:21:15 +0300

    fixed avatar bug

--- src/components/App/List/PersonalChatListItem.ts
+++ src/components/App/List/PersonalChatListItem.ts
@@ -9,19 +9,19 @@ class PersonalChatListItem extends PrivateChatListItem {
         return this.personalChat.userId
     }
     override get title(): string {
-        return this.users[this.userId].displayName ?? this.personalChat.title ?? ""
+        return this.users[this.userId]?.displayName ?? this.personalChat.title ?? ""
     }
     override get image(): string {
-        return this.users[this.userId].avatar ?? this.personalChat.image ?? ""
+        return this.users[this.userId]?.avatar ?? this.personalChat.image ?? ""
     }
     override get subtitle(): string | undefined {
-        return this.users[this.userId].textStatus ?? this.personalChat.userTextStatus
+        return this.users[this.userId]?.textStatus ?? this.personalChat.userTextStatus
     }
     get userStatus(): UserStatus {
-        return this.users[this.userId].status ?? this.personalChat.userStatus ?? UserStatus.Offline
+        return this.users[this.userId]?.status ?? this.personalChat.userStatus ?? UserStatus.Offline
     }
     constructor(private personalChat: PersonalChatLookUp, private users: {[id: string]: UserLookUp}) {
-        super(personalChat)
+        super(personalChat);
     }
 }
 export default PersonalChatListItem;
